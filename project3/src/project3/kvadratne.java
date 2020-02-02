2e79cea8e13d881d65ca5a0f40e15a8419c511c7
Максим <@DESKTOP-KN3DPMF>
2020-02-02 20:40:14 +0300

    3 encoding

--- project3/src/project3/kvadratne.java
+++ project3/src/project3/kvadratne.java
@@ -3,21 +3,21 @@ import java.util.Scanner;
 class kvadratne{
 public static void main (String args[]){
 	Scanner in = new Scanner(System.in);
-    System.out.print("Введите 3 числа a b c соответственно, через пробел и нажмите Enter: ");
+    System.out.print("vvedite a b c: ");
     float a = in.nextFloat(), b = in.nextFloat(), c = in.nextFloat();
     if(a == 0){
-        System.out.println("На 0 делить нельзя!!!");            
+        System.out.println("na 0 delit nelzya!!!");            
     }else{
     float d = (float) (Math.pow(b, 2) - (4*a*c));
     double x1 = (-b + Math.sqrt(d))/2*a;
     double x2 = (-b - Math.sqrt(d))/2*a;
     if(d < 0){
-    System.out.println("Нет корней!");
+    System.out.println("net korney!");
     }else if(d == 0){
-        System.out.print("Дискриминант: " + d);
-        System.out.println("; Корень уравнения: x = " + x1);
-    }else{System.out.print("Дискриминант: " + d);
-        System.out.print("; Корни уравнения: x1 = " + x1);
+        System.out.print("discriminant: " + d);
+        System.out.println("; x1 = " + x1);
+    }else{System.out.print("disriminant " + d);
+        System.out.print("; x1 = " + x1);
         System.out.println(", x2 = " + x2);
     }
     }
