a4b21d8b382435f812d5bcfa18cb17d7f28842e6
Максим <@DESKTOP-KN3DPMF>
2020-02-02 20:35:42 +0300

    3

--- project3/src/project3/kvadratne.java
+++ project3/src/project3/kvadratne.java
@@ -2,27 +2,23 @@ package project3;
 import java.util.Scanner;
 class kvadratne{
 public static void main (String args[]){
-Scanner sc = new Scanner(System.in);
-float a,b,c,d,x1,x2;
-System.out.print("Введите а: ");
-a = sc.nextFloat();
-System.out.print("Введите b: ");
-b = sc.nextFloat();
-System.out.print("Введите c: ");
-c = sc.nextFloat();
-d=(float)Math.pow(b,2)-(4*a*c);
-if (d<0){
-          System.out.println("Решения нет");
-          }
-          else { if (d==0) {
-                          x1=b/(-2*a);
-                          System.out.println("Одно решение х= "+x1);
-                          }
-                            else{
-                                x1=-b+(float)(Math.sqrt(d))/2*a;
-                                x2=-b-(float)(Math.sqrt(d))/2*a;
-                                System.out.println("x1= "+x1+" x2= "+x2);
-                                }
-                }
-}
+	Scanner in = new Scanner(System.in);
+    System.out.print("Введите 3 числа a b c соответственно, через пробел и нажмите Enter: ");
+    float a = in.nextFloat(), b = in.nextFloat(), c = in.nextFloat();
+    if(a == 0){
+        System.out.println("На 0 делить нельзя!!!");            
+    }else{
+    float d = (float) (Math.pow(b, 2) - (4*a*c));
+    double x1 = (-b + Math.sqrt(d))/2*a;
+    double x2 = (-b - Math.sqrt(d))/2*a;
+    if(d < 0){
+    System.out.println("Нет корней!");
+    }else if(d == 0){
+        System.out.print("Дискриминант: " + d);
+        System.out.println("; Корень уравнения: x = " + x1);
+    }else{System.out.print("Дискриминант: " + d);
+        System.out.print("; Корни уравнения: x1 = " + x1);
+        System.out.println(", x2 = " + x2);
+    }
+    }
 }
